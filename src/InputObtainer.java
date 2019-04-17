ea4089eae11210f5d0fdab194c59542304b18bec
alikhaledd <alikha1ed@outlook.com>
2019-04-17 14:10:19 +0200

    Scan the DOS Command

---
+++ src/InputObtainer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputObtainer {
+	
+	public static String keyboardScanner() {
+		
+		Scanner keyboardScanner = new Scanner(System.in);
+		String keyboardInput = keyboardScanner.nextLine();
+		keyboardScanner.close();
+		return keyboardInput;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Please Enter the DOS Command:");
+		String DOS_Command = InputObtainer.keyboardScanner();
+		System.out.println(DOS_Command);
+	}
+
+}
