50e8dcb7df215816b63a23306ab65c55ae39c40a
alikhaledd <alikha1ed@outlook.com>
2019-04-17 14:53:27 +0200

    getTheFirstWordOfTheCommand

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Please Enter the DOS Command:");
 		String DOS_Command = InputObtainer.keyboardScanner();
-		System.out.println(DOS_Command);
+		StringBuffer sb = StringHandler.getTheFirstWord(DOS_Command);
+		System.out.println(sb);
 	}
 
 }
