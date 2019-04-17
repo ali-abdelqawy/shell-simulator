3bd0cb6cd8a64d9a4ba0c423df606c47edf178d2
alikhaledd <alikha1ed@outlook.com>
2019-04-17 14:58:13 +0200

    Removing Extra Lines

--- src/InputObtainer.java
+++ src/InputObtainer.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class InputObtainer {
 	
 	public static String keyboardScanner() {
-		
 		Scanner keyboardScanner = new Scanner(System.in);
 		String keyboardInput = keyboardScanner.nextLine();
 		keyboardScanner.close();
--- src/StringHandler.java
+++ src/StringHandler.java
@@ -2,17 +2,14 @@
 public class StringHandler {
 	
 	public static StringBuffer getTheFirstWord(String str) {
-		
 		int spaceIndex = getFirstIndexOfSpaceChar(str);
 		return generateTheFirstWord(spaceIndex, str);
 	}
 	
 	private static int getFirstIndexOfSpaceChar(String str) {
-		
 		return str.indexOf(' ');
 	}
 	private static StringBuffer generateTheFirstWord(int spaceIndex, String str) {
-		
 		StringBuffer firstWord = new StringBuffer("");
 		for(int i = 0; i < spaceIndex; i++)
 		{
